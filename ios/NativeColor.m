729eb425202851954e3006b138a27f52952fbfcf
Jamon Holmgren <jamon@infinite.red>
2018-12-20 16:15:31 -0800

    v2 color

---
+++ ios/NativeColor.m
@@ -0,0 +1,20 @@
+//
+//  NativeColor.m
+//  RNP201812
+//
+//  Created by Jamon Holmgren on 12/20/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "NativeColor.h"
+
+@implementation NativeColor
+RCT_EXPORT_MODULE();
+
+RCT_REMAP_METHOD(bgColor,
+                 bgColorWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+  resolve(@"#00FF00");
+}
+@end
